8db53db99883ded3034c22b817f410ce68a0ce14
Timur-Malik <92898560+tim-fihost@users.noreply.github.com>
2023-09-27 11:49:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 response = requests.get(url=URL)
 html_text = response.text
-soup = BeautifulSoup(html_text)
+soup = BeautifulSoup(html_text,  'html.parser')
 movies = soup.select(selector='h3',class_="title" )
 movies = movies[::-1]
 for movie in movies:
